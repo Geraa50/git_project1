ec59fb18d2ba44b2ca12b246f07714733c6057fc
Geraa50 <78838055+Geraa50@users.noreply.github.com>
2023-02-03 09:24:06 +0600

    Update program.py
    i chnge with github

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git-repo')
     print('And I change it every day')
     print('New print')
+    print('I change this project use Github
 
 
 if __name__ == '__main__':
