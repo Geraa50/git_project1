895b39a798b66a8651e72bcc3db2e7a4b95e33e7
Geraa50 <gera.a130000000000000@gmail.com>
2023-05-01 20:31:09 +0600

    Update program.py

--- program.py
+++ program.py
@@ -1,9 +1,1 @@
-def main():
-    print('My first git-repo')
-    print('And I change it every day')
-    print('New print')
-    print('I change this project use Github
-
-
-if __name__ == '__main__':
-    main()
+print("Hello World")
